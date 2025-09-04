1d1284a0cc30141c8a68f800addcc4697b7aa6f1
Desock <salvagomezrisen@gmail.com>
2025-09-04 12:09:17 +0200

    header, principal content, experience y footer añadidos

--- Portfolio_Salva/ProyectoPortfolio/src/app/components/content/content.ts
+++ Portfolio_Salva/ProyectoPortfolio/src/app/components/content/content.ts
@@ -1,8 +1,14 @@
 import { Component } from '@angular/core';
+import { Profile } from '../profile/profile';
+import { Education } from '../education/education';
+import { Projects } from '../projects/projects';
+import { Skills } from '../skills/skills';
+import { Experience } from '../experience/experience';
+
 
 @Component({
   selector: 'app-content',
-  imports: [],
+  imports: [Profile, Education, Projects, Skills, Experience ],
   templateUrl: './content.html',
   styleUrl: './content.css'
 })
