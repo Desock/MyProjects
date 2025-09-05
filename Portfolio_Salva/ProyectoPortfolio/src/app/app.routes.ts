61cf73890df79300130a1fc7c2143c040b44520e
Desock <salvagomezrisen@gmail.com>
2025-09-05 12:52:49 +0200

    ajuste e implementacion de las rutas

--- Portfolio_Salva/ProyectoPortfolio/src/app/app.routes.ts
+++ Portfolio_Salva/ProyectoPortfolio/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { Profile } from './components/profile/profile';
+import { Experience } from './components/experience/experience';
+import { Projects } from './components/projects/projects';
+import { Education } from './components/education/education';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'profile', component: Profile },
+    { path: 'experience', component: Experience },
+    { path: 'projects', component: Projects },
+    { path: 'education', component: Education }
+];
--- Portfolio_Salva/ProyectoPortfolio/src/app/components/content/content.ts
+++ Portfolio_Salva/ProyectoPortfolio/src/app/components/content/content.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
-import { Profile } from '../profile/profile';
-import { Education } from '../education/education';
-import { Projects } from '../projects/projects';
-import { Skills } from '../skills/skills';
-import { Experience } from '../experience/experience';
+import { RouterOutlet } from '@angular/router';
+// import { Education } from '../education/education';
+// import { Projects } from '../projects/projects';
+// import { Skills } from '../skills/skills';
+// import { Experience } from '../experience/experience';
 
 
 @Component({
   selector: 'app-content',
-  imports: [Profile, Education, Projects, Skills, Experience ],
+  imports: [RouterOutlet],
   templateUrl: './content.html',
   styleUrl: './content.css'
 })
