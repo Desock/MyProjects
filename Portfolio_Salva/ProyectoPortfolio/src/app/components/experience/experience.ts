32bda08ec6676757d7631f4f8b7cfa29ababf534
Desock <salvagomezrisen@gmail.com>
2025-10-14 16:20:40 +0200

    Cambio completo del nav y pagina traducida

--- Portfolio_Salva/ProyectoPortfolio/src/app/components/experience/experience.ts
+++ Portfolio_Salva/ProyectoPortfolio/src/app/components/experience/experience.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { Header } from '../header/header';
 import { Footer } from '../footer/footer';
+import { TranslatePipe } from '@ngx-translate/core';
 
 
 @Component({
   selector: 'app-experience',
-  imports: [Header, Footer],
+  imports: [Header, Footer, TranslatePipe],
   templateUrl: './experience.html',
   styleUrl: './experience.css'
 })
